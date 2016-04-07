22abaa0658b9452a3416cf267b195907c231fb5e
Kris Redding <kredding@localhost.localdomain>
2016-04-07 18:45:42 -0400

    fix partial template

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,9 +1,9 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope){
   $scope.title = "Home";
-  
+
   $scope.sections = {
-    "s1": "",
-    "s2": ""
+    "s1": 'views/templates/_home1.html',
+    "s2": ''
   };
 
 });
