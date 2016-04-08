f3785fa899a98b8022ff7969888a4a41578f07ea
Kris Redding <kris.redding3@gmail.com>
2016-04-07 22:11:06 -0400

    add page structure

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,9 +1,16 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope){
   $scope.title = "Home";
 
-  $scope.sections = {
+  /*$scope.sections = {
     "s1": 'views/templates/_home1.html',
-    "s2": ''
-  };
+    "s2": 'views/templates/_services.html'
+  };*/
+  $scope.sections = [
+    {"id":"home1", "template":'views/templates/_home1.html'},
+    {"id":"teamwork", "template":'views/templates/_teamwork.html'},
+    {"id":"services", "template":'views/templates/_services.html'},
+    {"id":"investors", "template":'views/templates/_investors.html'},
+    {"id":"contactus", "template":'views/templates/_contactus.html'}
+  ];
 
 });
