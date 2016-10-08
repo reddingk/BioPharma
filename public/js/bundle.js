a47066078ce85cd89b7c0790760826008abad5c3
Kris Redding <kris.redding3@gmail.com>
2016-10-07 20:25:15 -0400

    1

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -46,6 +46,30 @@
 
 })();
 
+angular.module('directives', []).directive('sectionBottom', function ($window) {
+  return {
+    restrict: 'EA',
+    link: function ($scope, element, attrs) {
+
+      angular.element($window).bind("scroll", function() {
+        var bottomSection = angular.element(document.getElementsByClassName("mainBody"))[0].children[1].offsetTop;
+        var windowp = angular.element($window)[0];
+        var invPop = angular.element(document.getElementsByClassName("investorsPop"));
+
+        if((windowp.innerHeight == bottomSection) || (windowp.pageYOffset > (bottomSection - this.innerHeight))){
+          if(!invPop.hasClass("screenPass"))
+            invPop.addClass("screenPass");
+        }
+        else {
+          if(invPop.hasClass("screenPass")){
+            invPop.removeClass("screenPass");
+          }
+        }
+      });
+    }
+  }
+});
+
 (function(){
    "use strict";
 
@@ -133,27 +157,3 @@
     }]);
 
 })();
-
-angular.module('directives', []).directive('sectionBottom', function ($window) {
-  return {
-    restrict: 'EA',
-    link: function ($scope, element, attrs) {
-
-      angular.element($window).bind("scroll", function() {
-        var bottomSection = angular.element(document.getElementsByClassName("mainBody"))[0].children[1].offsetTop;
-        var windowp = angular.element($window)[0];
-        var invPop = angular.element(document.getElementsByClassName("investorsPop"));
-
-        if((windowp.innerHeight == bottomSection) || (windowp.pageYOffset > (bottomSection - this.innerHeight))){
-          if(!invPop.hasClass("screenPass"))
-            invPop.addClass("screenPass");
-        }
-        else {
-          if(invPop.hasClass("screenPass")){
-            invPop.removeClass("screenPass");
-          }
-        }
-      });
-    }
-  }
-});
