8a7020a4afb6e9ae1e086a609ef248931860277c
Kris Redding <kris.redding3@gmail.com>
2016-10-10 10:05:00 -0400

    style updates

--- app/js/controllers/homeCtrl.js
+++ app/js/controllers/homeCtrl.js
@@ -5,7 +5,7 @@
       var vm = this;
       vm.title = "Home";
       vm.test = "/img/backgrounds/6.jpg";
-      
+
       vm.sections = [
         {"id":"main", "template":'views/templates/_main.html'},
         {"id":"aboutus", "template":'views/templates/pages/_aboutus.html'},
@@ -15,12 +15,12 @@
         {"id":"contactus", "template":'views/templates/pages/_contactus.html'},
       ];
       vm.aboutInfo = [
-        {"title":"Overview", "type":"text", "isopen":false, "content":""},
+        {"title":"Overview", "type":"text", "isopen":false, "content":"JK BioPharma Solutions, Inc. (JKBP) was established in January 2013 pursuing to be a sophisticated outstanding developer in new drug development. We develop drug candidates throughout early stage to human proof-of-concept to maximize their value, which enables successful out-licensing to global leading pharmaceuticals. We accelerate drug development by innovation, out-sourcing and key personnel. We establish a substantial system to work with start-up biotech companies as well as established Bio/Pharmaceutical foreign companies pursuing globalization, especially the US market."},
         {"title":"Board Of Directors", "type":"list", "isopen":false, "content":[{"name":"Richard J. Kang, PhD", "title":"Molecular & Cell Biology", "resume":["Founder of JK BioPharma Solutions, Inc", "Presently Board of Director & Chief Operating Officer at NeoImmuneTech, Inc (Maryland)","Leader Group Mentors of Korea Innovation Center (KIC) for Ministry of Science, ICT and Future Planning (MSIP) of Korea", "Formerly Chief Executive Officer at NeoImmuneTech, Inc","PhD from the University of Edinburgh","Serial entrepreneur, experience in Management, Operation, R&D, Clinical Development and Business Development in Bio/Pharma companies", "Various research experience for 17 years (20 publications in Cell, Nature, J of Pathology etc.; 9 patents)" ]},{"name":"Sun Dae Kang, PhD", "title":"Economics", "resume":["Formerly Chairman at Nexol Investments", "Formerly Adjunctive Faculty at Myungji University", "Formerly the first Head of Economic Research Institute at Kyobo Life Insurance", "Formerly Vice President Kyobo Life Insurance", "Formerly Vice President at Hanshin Securities", "Formerly Director at Daewoo Economic Research Institute", "PhD from Yunsei University", "Depth of expertise across multiple economic domains including venture capitals, securities etc." ]} ]},
-        {"title":"Special Advisors", "type":"text", "isopen":false, "content":""}
+        {"title":"Special Advisors", "type":"text", "isopen":false, "content":"JK BioPharma Solutions, Inc. organizes a project-specialized scientific advisory group when we commit the project with a collaborator. The special advisory group consisting of substantially experienced experts in the area of the specified indication of a pipeline provides the most effective strategy to the project."}
       ];
 
-      vm.EModels = [{"title":"Strategic Plan", "img":"models/Strategic_Plan.png", "isopen":false},{"title":"Innovation Management", "img":"models/Innovative_management.png", "isopen":false},{"title":"Business Model", "img":"models/Business_Model.png", "isopen":false}];
+      vm.EModels = [{"title":"Strategic Plan", "img":"models/Strategic_Plan.png", "isopen":false},{"title":"Innovative Management", "img":"models/Innovative_management.png", "isopen":false},{"title":"Business Model", "img":"models/Business_Model.png", "isopen":false}];
 
       vm.contactUsForm = {"name":"","phone":"","toEmail":"","subject":"","message":""};
       var SGKey = "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";
@@ -66,19 +66,6 @@
         var returnParams = "?api_user=jkbiopharma" +"&api_key="+SGKey+"&to=info@jkbiopharma.com&toname=JKBioPharma"+"&subject="+ fullSubject + "&text="+ fullText+"&from=info@jkbiopharma.com";
         return returnParams;
       }
-      // Demo Pages
-      vm.sections_old = [
-        {"id":"main", "template":'views/templates/_main.html'},
-        {"id":"teamwork", "template":'views/templates/demo/_teamwork.html'},
-        {"id":"services", "template":'views/templates/demo/_services.html'},
-        {"id":"investors", "template":'views/templates/demo/_investors.html'},
-        {"id":"contactus", "template":'views/templates/pages/_contactus.html'}
-      ];
-      vm.services = [
-        {"title":"Nemo enim ipsam voluptatem", "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."},
-        {"title":"Excepteur sint occaecat cupidatat", "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."},
-        {"title":"Duis aute irure dolor in reprehenderit", "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
-      ];
 
     }]);
 
